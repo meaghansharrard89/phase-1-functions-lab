015f431bf5b9ea9fb49e30be1043272a85a7f30e
Meaghan <meaghan.sharrard89@gmail.com>
2023-10-30 21:25:19 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+let headquarters = 42;
+function distanceFromHqInBlocks(distance) {
+       return Math.abs(distance - headquarters);
+    }
+
+function distanceFromHqInFeet(distance2) {
+    return distanceFromHqInBlocks(distance2) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let result = distanceTravelledInFeet(start, destination);
+    if (result <= 400) {
+        return 0;
+    } else if (result <= 2000) {
+        return (result - 400) * .02;
+    } else if (result <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
